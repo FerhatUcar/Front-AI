5bbd8d223ef1d37565ca9efe1d122ecc14406ffa
ferhatucar <ucarferhat3@gmail.com>
2019-12-11 11:27:55 +0100

    removes feedback column

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, Fragment} from 'react';
+import React, {useState} from 'react';
 import ReactFileReader from 'react-file-reader';
 import { CsvToHtmlTable } from 'react-csv-to-table';
 
@@ -6,8 +6,6 @@ import './assets/scss/App.scss';
 
 const App = () => {
     const [csvData, setCsvData] = useState(null);
-
-    const rows = [1, 2, 3];
 
     const handleFiles = files => {
         let reader = new FileReader();
@@ -23,27 +21,11 @@ const App = () => {
                 </ReactFileReader>
                 <div className="box__table">
                     {csvData && (
-                        <Fragment>
-                            <CsvToHtmlTable
-                                data={csvData}
-                                csvDelimiter=";"
-                                tableClassName="table"
-                            />
-                            <table className="box__feedback">
-                                <thead className="box__feedback--head">
-                                    <tr><th>Feedback</th></tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        {rows.map(row => (
-                                            <td key={row}>
-                                                <i className="far fa-smile" />
-                                            </td>
-                                        ))}
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </Fragment>
+                        <CsvToHtmlTable
+                            data={csvData}
+                            csvDelimiter=";"
+                            tableClassName="table"
+                        />
                     )}
                 </div>
             </div>
