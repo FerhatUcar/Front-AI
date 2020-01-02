36d6912bbae366b6282d08700ab673f577c74f04
FerhatUcar <ucarferhat3@gmail.com>
2020-01-02 12:50:34 +0100

    adds read front component with upload

--- src/components/read/Read.js
+++ src/components/read/Read.js
@@ -1,9 +1,21 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
+import ReactFileReader from "react-file-reader";
 
 const Read = () => {
+    // eslint-disable-next-line
+    const [data, setData] = useState(null);
+
+    const handleFiles = files => {
+        let reader = new FileReader();
+        reader.onload = () => setData(reader.result);
+        reader.readAsText(files[0]);
+    };
+
     return (
         <Fragment>
-            <p className="white">Read front app here</p>
+            <ReactFileReader handleFiles={handleFiles} fileTypes={'.txt'}>
+                <button className='box__btn'>Upload</button>
+            </ReactFileReader>
         </Fragment>
     );
 };
